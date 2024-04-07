3e03326d1ad56e929b7b453b436d6ace8a990a4a
Mohammad Amber Alam <amberalam.3101@gmail.com>
2024-04-07 17:11:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [ans, setAns] = useState(null);
+  const inputRef = useRef();
+
+  const inputArr = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"];
+
+  const clickHandler = (e) => {
+    console.log(e.target.innerText);
+    if (e.target.innerText === "=") {
+      if (inputRef.current.value === "") {
+        setAns("Error");
+        return;
+      }
+
+      setAns(eval(inputRef.current.value));
+      // setAns(eval(e.target.innerText));
+    } else if (e.target.innerText === "C") {
+      setInputText("");
+      setAns("");
+    } else {
+      setInputText((prevState) => prevState + e.target.innerText);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calcContainer">
+      <h1>React Calculator</h1>
+      <input ref={inputRef} type="text" value={inputText} />
+
+      {ans && <div className="ans">{ans}</div>}
+
+      <div
+        className="calcButtons"
+        onClick={(e) => {
+          clickHandler(e);
+        }}
+      >
+        {inputArr.map((input) => {
+          return (
+            <button id={input} className="numberButtons">
+              {input}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
